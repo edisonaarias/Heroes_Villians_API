73debdb3d925b0df73bca1f916601c6bd8f5cd4d
unknown <edisonalexanderarias@gmail.com>
2022-05-31 11:18:26 -0700

    Added to teh urls file and views file

--- heroes_villains/urls.py
+++ heroes_villains/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.heroes_villains_list),
+    path('<int:pk>/', views.hero_villain_detail),
 ]
--- heroes_villains/views.py
+++ heroes_villains/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import Hero_VillainSerializer
 from .models import Hero_Villain
+from heroes_villains import serializers
 
 @api_view(['GET','POST'])
 def heroes_villains_list(request):
@@ -15,3 +17,16 @@ def heroes_villains_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view (['GET', 'PUT'])
+def hero_villain_detail(request, pk):
+    hero_villain = get_object_or_404(Hero_Villain,pk=pk)
+    if request.method == 'GET':
+        serializer = Hero_VillainSerializer(hero_villain);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+          serializer = Hero_VillainSerializer(Hero_Villain, data=request.data)
+          serializer.is_valid(raise_exception=True)
+          serializer.save()
+          return Response(serializer.data)
